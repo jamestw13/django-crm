d5dc63bfee2a0a2282e87e9c228a974ade8d7dfe
TJ James <jamestw13@gmail.com>
2023-08-23 11:41:13 -0500

    implement add user functionality and page

--- website/views.py
+++ website/views.py
@@ -1,8 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
-from .forms import SignUpForm
-# , AddRecordForm
+from .forms import SignUpForm, AddRecordForm
 from .models import Record
 
 
@@ -74,18 +73,18 @@ def delete_record(request, pk):
 		return redirect('home')
 
 
-# def add_record(request):
-# 	form = AddRecordForm(request.POST or None)
-# 	if request.user.is_authenticated:
-# 		if request.method == "POST":
-# 			if form.is_valid():
-# 				add_record = form.save()
-# 				messages.success(request, "Record Added...")
-# 				return redirect('home')
-# 		return render(request, 'add_record.html', {'form':form})
-# 	else:
-# 		messages.success(request, "You Must Be Logged In...")
-# 		return redirect('home')
+def add_record(request):
+	form = AddRecordForm(request.POST or None)
+	if request.user.is_authenticated:
+		if request.method == "POST":
+			if form.is_valid():
+				add_record = form.save()
+				messages.success(request, "Record Added...")
+				return redirect('home')
+		return render(request, 'add_record.html', {'form':form})
+	else:
+		messages.success(request, "You Must Be Logged In...")
+		return redirect('home')
 
 
 # def update_record(request, pk):

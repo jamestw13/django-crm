2bee472fd35cac32e5ed2e1eddcd9e9378a3f48b
TJ James <jamestw13@gmail.com>
2023-08-23 11:28:18 -0500

    filled out single record page

--- website/views.py
+++ website/views.py
@@ -52,14 +52,14 @@ def register_user(request):
 
 
 
-# def customer_record(request, pk):
-# 	if request.user.is_authenticated:
-# 		# Look Up Records
-# 		customer_record = Record.objects.get(id=pk)
-# 		return render(request, 'record.html', {'customer_record':customer_record})
-# 	else:
-# 		messages.success(request, "You Must Be Logged In To View That Page...")
-# 		return redirect('home')
+def customer_record(request, pk):
+	if request.user.is_authenticated:
+		# Look Up Records
+		customer_record = Record.objects.get(id=pk)
+		return render(request, 'record.html', {'customer_record':customer_record})
+	else:
+		messages.success(request, "You Must Be Logged In To View That Page...")
+		return redirect('home')
 
 
 

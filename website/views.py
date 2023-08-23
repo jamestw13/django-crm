a3a1bc2a28b6dc81c719a24b6c69a763538b1a36
TJ James <jamestw13@gmail.com>
2023-08-23 11:32:23 -0500

    implemented record deletion

--- website/views.py
+++ website/views.py
@@ -63,15 +63,15 @@ def customer_record(request, pk):
 
 
 
-# def delete_record(request, pk):
-# 	if request.user.is_authenticated:
-# 		delete_it = Record.objects.get(id=pk)
-# 		delete_it.delete()
-# 		messages.success(request, "Record Deleted Successfully...")
-# 		return redirect('home')
-# 	else:
-# 		messages.success(request, "You Must Be Logged In To Do That...")
-# 		return redirect('home')
+def delete_record(request, pk):
+	if request.user.is_authenticated:
+		delete_it = Record.objects.get(id=pk)
+		delete_it.delete()
+		messages.success(request, "Record Deleted Successfully...")
+		return redirect('home')
+	else:
+		messages.success(request, "You Must Be Logged In To Do That...")
+		return redirect('home')
 
 
 # def add_record(request):
